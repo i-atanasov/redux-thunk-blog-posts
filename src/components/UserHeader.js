63154a8c05be342f5870899be6b6931f76b5eba4
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-17 18:45:03 +0300

    fetch memoization

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
+
 class UserHeader extends React.Component {
     componentDidMount() {
         this.props.fetchUser(this.props.userId);
     }
 
     render() {
+        const { user } = this.props;
 
-        const user = this.props.users.find((user) => user.id === this.props.userId);
         if (!user) {
             return null;
         };
@@ -21,8 +22,8 @@ class UserHeader extends React.Component {
         )};
 };
 
-const mapStateToProps = (state) => {
-    return { users: state.users };
+const mapStateToProps = (state, ownProps) => {    
+    return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
