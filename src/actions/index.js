86d0c59dd948a7fb279bdec1f346ae6c50b55a63
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-17 19:10:14 +0300

    nested actions requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,12 +7,25 @@ export const fetchPosts = () => async dispatch => {
         dispatch({ type: 'FETCH_POSTS', payload: res.data });
     };
 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
+   
     const res = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({ type: 'FETCH_USER', payload: res.data });
-});
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const res = await jsonPlaceholder.get(`/users/${id}`)
+
+//     dispatch({ type: 'FETCH_USER', payload: res.data });
+// });
