260cc7114942b56272ff8fc8999c33270fd0180d
i-atanasov <ivayloatanasov@zohomail.eu>
2022-05-17 18:09:36 +0300

    single user requests

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
-const UserHeader = () => {
-    return (
-        <div>
-            UserHeader
-        </div>
-    )
+class UserHeader extends React.Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.userId);
+    }
+
+    render() {
+
+        const user = this.props.users.find((user) => user.id === this.props.userId);
+        if (!user) {
+            return null;
+        };
+
+        return (
+            <div className='header'>
+                {user.name}
+            </div>
+        )};
 };
 
-export default UserHeader;
+const mapStateToProps = (state) => {
+    return { users: state.users };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
